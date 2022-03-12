abc40e7846518f84286aeef7d4eb0ca607526e8f
Humberto Martins <humberto@consilio.com.br>
2022-03-12 14:15:55 -0300

    chore: set api

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:4000',
+  baseURL: process.env.API_URL || 'https://truephone-server.herokuapp.com/',
 });
 
 api.interceptors.request.use(
